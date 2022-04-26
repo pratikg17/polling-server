3e600193401f33c8554ba0b572714cbef9358e22
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-26 13:06:03 -0400

    Added migration

---
+++ migrations/sqls/20220426170111-add-votes-table-up.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS votes;
+CREATE TABLE votes (
+ vote_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+ poll_option_id UUID NOT NULL,
+ poll_id UUID NOT NULL,
+ user_id UUID NOT NULL,
+ created_at timestamptz NOT NULL DEFAULT now(),
+ updated_at timestamptz NOT NULL DEFAULT now(),
+ CONSTRAINT fk_users_votes FOREIGN KEY(user_id) REFERENCES users(user_id),
+ CONSTRAINT fk_polls_votes FOREIGN KEY(poll_option_id) REFERENCES polls(poll_id),
+ CONSTRAINT fk_poll_options_votes FOREIGN KEY(poll_option_id) REFERENCES poll_options(poll_option_id)
+);
