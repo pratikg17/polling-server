b416d48abeaafa626d890dbeb71532d543337f0d
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-26 12:59:17 -0400

    Added Poll Option table

---
+++ migrations/sqls/20220426165627-add-poll-option-table-up.sql
@@ -0,0 +1,11 @@
+/* Replace with your SQL commands */
+DROP TABLE IF EXISTS poll_options;
+CREATE TABLE poll_options (
+ poll_option_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+ option_name TEXT NOT NULL UNIQUE,
+ color TEXT NOT NULL UNIQUE,
+ poll_id UUID NOT NULL,
+ created_at timestamptz NOT NULL DEFAULT now(),
+ updated_at timestamptz NOT NULL DEFAULT now(),
+ CONSTRAINT fk_poll_options_polls FOREIGN KEY(poll_id) REFERENCES polls(poll_id)
+);
