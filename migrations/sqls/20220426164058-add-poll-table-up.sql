7681814180e608e7778d5af4d714be3272d9f18d
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-26 12:45:35 -0400

    Added poll table migration

---
+++ migrations/sqls/20220426164058-add-poll-table-up.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS polls;
+CREATE TABLE polls (
+ poll_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+ poll_name TEXT NOT NULL UNIQUE,
+ poll_desc TEXT NOT NULL,
+ user_id UUID NOT NULL,
+ is_featured BOOLEAN,
+ created_at timestamptz NOT NULL DEFAULT now(),
+ updated_at timestamptz NOT NULL DEFAULT now(),
+ CONSTRAINT fk_polls_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
